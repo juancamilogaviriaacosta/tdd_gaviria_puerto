70ac51c39da5796cdeb20fa47550a85f49e90886
juan <juancamilogaviriaacosta@gmail.com>
2017-03-21 22:15:06 -0500

    Color verde, test para numero de elementos de un arreglo con un elemento

--- Calculadora.py
+++ Calculadora.py
@@ -6,3 +6,5 @@ class Calculadora:
     def numeroElementos(self, arreglo):
         if arreglo is None or len(arreglo.strip()) == 0:
             return 0
+        else:
+            return len(arreglo.strip())
