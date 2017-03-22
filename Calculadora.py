2a6edf3a5974fa6135e1e1b9f6ba5e79938c84a6
mpuertog <ms.puerto@uniandes.edu.co>
2017-03-21 22:55:29 -0500

    color verde, arreglo vacio con el número de elementos y el mínimo

--- Calculadora.py
+++ Calculadora.py
@@ -2,9 +2,12 @@ import math
 
 
 class Calculadora:
-
     def numeroElementos(self, arreglo):
         if arreglo is None or len(arreglo.strip()) == 0:
             return 0
         else:
             return len(arreglo.split(','))
+
+    def numeroElementosYMinimo(self, arreglo):
+        if arreglo is None or len(arreglo.strip()) == 0:
+            return (0, 0)
