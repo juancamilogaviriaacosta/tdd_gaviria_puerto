c398a30d7680bc305095d6dd438d8f7ec6370038
juan <juancamilogaviriaacosta@gmail.com>
2017-03-21 22:03:51 -0500

    Color verde, test para numero de elementos de un arreglo nulo o vacio

--- test_solver.py
+++ test_solver.py
@@ -2,11 +2,10 @@ from unittest import TestCase
 from Calculadora import Calculadora
 
 class TestSolver(TestCase):
+
     def test_demo(self):
+
         c = Calculadora()
 
-        c.calcular(3, 3, 2)
-
-        suma = c.sumar(3, 7)
-
-        self.assertEqual(suma, 10)
+        self.assertEquals(0, c.numeroElementos(None))
+        self.assertEquals(0, c.numeroElementos(""))
